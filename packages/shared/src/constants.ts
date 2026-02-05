58563d24c09eb87ac679e969fb9b6ba809f5929f
Charles Denault <charles@charlesdenault.com>
2026-02-05 12:26:33 -0600

    Add TURN servers for better NAT traversal

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -2,6 +2,27 @@ export const ICE_SERVERS: RTCIceServer[] = [
   { urls: 'stun:stun.l.google.com:19302' },
   { urls: 'stun:stun1.l.google.com:19302' },
   { urls: 'stun:stun2.l.google.com:19302' },
+  { urls: 'stun:stun.relay.metered.ca:80' },
+  {
+    urls: 'turn:global.relay.metered.ca:80',
+    username: 'e8dd65b92f6389a52ce6c487',
+    credential: 'pWSLdHIzG+WBCdFA',
+  },
+  {
+    urls: 'turn:global.relay.metered.ca:80?transport=tcp',
+    username: 'e8dd65b92f6389a52ce6c487',
+    credential: 'pWSLdHIzG+WBCdFA',
+  },
+  {
+    urls: 'turn:global.relay.metered.ca:443',
+    username: 'e8dd65b92f6389a52ce6c487',
+    credential: 'pWSLdHIzG+WBCdFA',
+  },
+  {
+    urls: 'turns:global.relay.metered.ca:443?transport=tcp',
+    username: 'e8dd65b92f6389a52ce6c487',
+    credential: 'pWSLdHIzG+WBCdFA',
+  },
 ];
 
 export const MAX_ROOM_SIZE = 6;
